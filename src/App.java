aecf31627aa3b2bb7b3492d2ff678607c31dc0f4
MaxKnowsAll <131699519+MaxKnowsAll@users.noreply.github.com>
2024-05-01 11:13:58 -0400

    maxs turn done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import javax.swing.JOptionPane;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    JOptionPane.showMessageDialog(null, "WELCOME TO X GAMES!!!!");
+    String password = JOptionPane.showInputDialog( "Please enter your password");
+    if(password == "the"){
+        JOptionPane.showMessageDialog(null, " WOW you are so smart WOOOHOOOO!!");
+    }
+    else {
+        JOptionPane.showMessageDialog(null, "Whoops thats not your password try again");
+    }
     }
 }
+//question 1 Image of triangle answer with text box. The question is find X. The answer for x will be the X button that closes the program.
+
+//question 2
+//question 3 
